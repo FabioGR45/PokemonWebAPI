cd8f5f143de6e27372c08d6514273e93be273ef8
Fábio Garcia Rios <fabiogarcia45@gmail.com>
2022-08-29 21:13:47 -0300

    Unificando versão Json em branch separada
    Adicionando o projeto versão JSON em uma branch separada e própria no repositório principal

--- WebApiProject/WebApiProject/Controllers/PokemonController.cs
+++ WebApiProject/WebApiProject/Controllers/PokemonController.cs
@@ -4,9 +4,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+using System.Xml.Linq;
 
 namespace WebApiProject.Controllers
 {
@@ -25,73 +29,111 @@ namespace WebApiProject.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Pokemon>>> Get()
         {
-            return Ok(await _context.Pokemons.ToListAsync());
+
+            using var reader = new StreamReader(".\\data.json");
+            var json = await reader.ReadToEndAsync();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+
+            //return Ok(await _context.Pokemons.ToListAsync());
+            return Ok(data);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Pokemon>> Get(int id)
         {
 
-            var pokemon = await _context.Pokemons.ToListAsync();
+            //var pokemon = await _context.Pokemons.ToListAsync();
 
-            var result = pokemon.Where(x => x.Id == id);
+            using var reader = new StreamReader(".\\data.json");
+            var json = await reader.ReadToEndAsync();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+
+            var result = data.Where(x => x.Id == id);
 
             //var pokemon = await _context.Pokemons.FindAsync(id);
-
+            
             if (result == null)
                 return BadRequest("Pokémon não encontrado");
-
+            
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Pokemon>>> AddPokemon([FromBody] Pokemon pkm)
+        public async Task<ActionResult<List<Pokemon>>> AddPokemon([FromBody]Pokemon pkm)
         {
-            _context.Pokemons.Add(pkm);
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Pokemons.ToListAsync());
+            var reader = new StreamReader(".\\data.json");
+            var json = reader.ReadToEnd();
+            reader.Dispose();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+
+            data.Add(pkm);
+            var content = JsonSerializer.Serialize(data);
+            System.IO.File.WriteAllText(".\\data.json", content);
+            //await _context.SaveChangesAsync();
+            return Ok(data);
         }
 
         [HttpPut]
         public async Task<ActionResult<List<Pokemon>>> UpdatePokemon(Pokemon request)
         {
+
+            var reader = new StreamReader(".\\data.json");
+            var json = reader.ReadToEnd();
+            reader.Dispose();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+
             var dbPokemon = await _context.Pokemons.FindAsync(request.Id);
 
-            if (dbPokemon == null)
-                return BadRequest("Pokémon não encontrado");
+            var pokeUpdate = data.Where(x => x.Id == request.Id).First();
 
-            dbPokemon.Name = request.Name;
-            dbPokemon.Type = request.Type;
-            dbPokemon.Region = request.Region;
-            dbPokemon.Hp = request.Hp;
-            dbPokemon.Attack = request.Attack;
+            data.Remove(pokeUpdate);
 
-            await _context.SaveChangesAsync();
+            var changedPokemon = new Pokemon
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Type = request.Type,
+                Region = request.Region,
+                Hp = request.Hp,
+                Attack = request.Attack
+            };
 
-            return Ok(await _context.Pokemons.ToListAsync());
+            data.Add(changedPokemon);
+            var content = JsonSerializer.Serialize(data);
+            System.IO.File.WriteAllText(".\\data.json", content);
+
+            return Ok(data);
         }
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<List<Pokemon>>> DeletePokemon(int id)
+
+        [HttpDelete("remove")]
+        public async Task<ActionResult<List<Pokemon>>> DeletePokemon([FromBody] Pokemon pkm)
         {
-            var dbPokemon = await _context.Pokemons.FindAsync(id);
 
-            if (dbPokemon == null)
-                return BadRequest("Pokémon não encontrado");
+            var reader = new StreamReader(".\\data.json");
+            var json = reader.ReadToEnd();
+            reader.Dispose();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
 
-            _context.Pokemons.Remove(dbPokemon);
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Pokemons.ToListAsync());
+            data.Remove((Pokemon)data.Where(x => x.Id == pkm.Id).First());
+            var content = JsonSerializer.Serialize(data);
+            System.IO.File.WriteAllText(".\\data.json", content);
+            //await _context.SaveChangesAsync();
+            return Ok(data);
         }
+        
 
         //Busca Região
         [HttpGet("LinqRegion")]
-        public async Task<ActionResult<List<Pokemon>>> GetPokeRegion([FromQuery] string filter, [FromQuery] string orderBy)
-        {
+        public async Task<ActionResult<List<Pokemon>>> GetPokeRegion([FromQuery]string filter, [FromQuery] string orderBy)
+            {
 
-            var pokemon = await _context.Pokemons.ToListAsync();
+            using var reader = new StreamReader(".\\data.json");
+            var json = await reader.ReadToEndAsync();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+            //var pokemon = await _context.Pokemons.ToListAsync();
 
-            var result = pokemon.Where(x => x.Region == filter);
+            var result = data.Where(x => x.Region == filter);
 
             var filteredData = result;
 
@@ -131,9 +173,13 @@ namespace WebApiProject.Controllers
         [HttpGet("LinqType")]
         public async Task<ActionResult<List<Pokemon>>> GetPokeType([FromQuery] string filter, [FromQuery] string orderBy)
         {
-            var pokemon = await _context.Pokemons.ToListAsync();
 
-            var result = pokemon.Where(x => x.Type.Contains(filter));
+            using var reader = new StreamReader(".\\data.json");
+            var json = await reader.ReadToEndAsync();
+            var data = JsonSerializer.Deserialize<List<Pokemon>>(json);
+            //var pokemon = await _context.Pokemons.ToListAsync();
+
+            var result = data.Where(x => x.Type.Contains(filter));
 
             var filteredData = result;
 
@@ -171,7 +217,7 @@ namespace WebApiProject.Controllers
 
 
         [HttpGet("Battle_TesteAlfa")]
-        public async Task<ActionResult<List<Pokemon>>> GetBattle([FromQuery] int id1, [FromQuery] int id2)
+        public async Task<ActionResult<List<Pokemon>>> GetBattle([FromQuery]int id1, [FromQuery] int id2)
         {
 
             var poke1 = await _context.Pokemons.FindAsync(id1);
@@ -180,26 +226,21 @@ namespace WebApiProject.Controllers
             var Poke1Battlepoints = poke1.Hp + poke1.Attack;
             var Poke2Battlepoints = poke2.Hp + poke2.Attack;
 
-            if (Poke1Battlepoints > Poke2Battlepoints)
-            {
+            if(Poke1Battlepoints > Poke2Battlepoints) { 
                 return Ok(new
                 {
                     StatusCode = 200,
                     Message = $"Pokémon {poke1.Name} venceu!",
                     Data = poke1
                 });
-            }
-            else if (Poke1Battlepoints < Poke2Battlepoints)
-            {
+            } else if(Poke1Battlepoints < Poke2Battlepoints) {
                 return Ok(new
                 {
                     StatusCode = 200,
                     Message = $"Pokémon {poke2.Name} venceu!",
                     Data = poke2
                 });
-            }
-            else
-            {
+            } else {
                 return Ok(new
                 {
                     StatusCode = 200,
@@ -208,104 +249,5 @@ namespace WebApiProject.Controllers
                 });
             }
         }
-
-        [HttpGet("Get_Status")]
-        public async Task<ActionResult<List<Pokemon>>> GetHp()
-        {
-
-            var pokemon = await _context.Pokemons.ToListAsync();
-
-            var totalHP = 0;
-            var mediumHP = 0;
-
-            var totalAttack = 0;
-            var mediumAttack = 0;
-
-            var totalPoke = 0;
-
-            //MAIOR HP
-            var highestHP = int.MinValue;
-            var HighestHpName = "";
-
-
-            //MAIOR ATAQUE
-            var highestAttack = int.MinValue;
-            var HighestAttackName = "";
-
-            //LOOP
-            var breakLoop = true;
-
-            var resultHP = pokemon.Where(x => x.Hp > totalHP).ToList();
-
-            while (breakLoop)
-            {
-
-                foreach (var poke in resultHP)
-                {
-                    if(poke.Hp > highestHP)
-                    {
-
-                        highestHP = poke.Hp;
-                        HighestHpName = poke.Name;
-
-                    }
-                }
-
-                breakLoop = false;
-
-            }
-
-            breakLoop = true;
-
-            var resultAttack = pokemon.Where(x => x.Attack > totalAttack).ToList();
-
-            while (breakLoop)
-            {
-
-                foreach (var poke in resultAttack)
-                {
-                    if (poke.Attack > highestAttack)
-                    {
-
-                        highestAttack = poke.Attack;
-                        HighestAttackName = poke.Name;
-
-                    }
-                }
-
-                breakLoop = false;
-
-            }
-
-
-
-            for(int i = 0; i < pokemon.Count; i++)
-            {
-                totalPoke++;
-            }
-
-            //TOTAL DE HP
-            foreach (var poke in resultHP)
-            {
-                totalHP = totalHP + poke.Hp;
-            }
-
-            //MÉDIA DE HP
-            mediumHP = totalHP / totalPoke;
-
-            //TOTAL DE ATAQUE
-            foreach (var poke in resultAttack)
-            {
-                totalAttack = totalAttack + poke.Attack;
-            }
-
-            //MÉDIA DE ATAQUE
-            mediumAttack = totalAttack / totalPoke;
-
-            return Ok($"HP TOTAL = {totalHP} \nMÉDIA DE HP = {mediumHP} \nMAIOR HP = {highestHP} (Pokémon: {HighestHpName}) \n\nATAQUE TOTAL = {totalAttack} \nMÉDIA DE ATAQUE = {mediumAttack} " +
-                $"\nMAIOR ATAQUE = {highestAttack} (Pokémon: {HighestAttackName})");
-
-        }
-
     }
 }
